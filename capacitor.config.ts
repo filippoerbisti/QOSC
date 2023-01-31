4c0ddbc7e38cbd92970847b65406fe560cf17a50
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-01-31 08:34:20 +0100

    config capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'music',
+  appId: 'app.vercel.qosc',
+  appName: 'QOSC',
   webDir: 'out',
   bundledWebRuntime: false,
   server: {
