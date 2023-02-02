315a76b76a52a96f3ee0c59f77fdb8959c06eff8
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-02 14:43:01 +0100

    fix manage notifications

--- components/pages/Notifications.jsx
+++ components/pages/Notifications.jsx
@@ -12,23 +12,22 @@ import {
   IonLabel,
   IonText,
 } from '@ionic/react';
-import Store from '../../store';
-import { getNotifications } from '../../store/selectors';
 import { close } from 'ionicons/icons';
 import { useState } from 'react';
 
-const Notifications = ({ open, onDidDismiss }) => {
-  const notifics = Store.useState(getNotifications)
+const Notifications = ({ notifics, open, onDidDismiss }) => {
+  // const notifics = Store.useState(getNotifications)
   // Order by notifications
   notifics.sort(function(x, y){
     return y.when - x.when;
   })
-  const [notifications, setNotifications] = useState(notifics)
+
+  const [notifications, setNotifications] = useState([...notifics])
   const pastTime = []
 
-  for (var i = 0; i < notifications.length; i++) {
+  for (var i = 0; i < notifics.length; i++) {
     let now = new Date()
-    let dateNotification = new Date(notifications[i].when)
+    let dateNotification = new Date(notifics[i].when)
     let difference = now.getTime() - dateNotification.getTime()
     let daysDifference = Math.floor(difference/1000/60/60/24);
       difference -= daysDifference*1000*60*60*24
@@ -78,16 +77,16 @@ const Notifications = ({ open, onDidDismiss }) => {
           </IonToolbar>
         </IonHeader> */}
         <div className='flex flex-col'>
-          {notifications.length > 0 && <>
+          {notifics.length > 0 && <>
               <IonText color="medium" className='pr-2 pt-1 text-right underline cursor-pointer' onClick={() => clearAllNotif()} style={{color: 'var(--ion-color-primary)'}}>Elimina tutte le notifiche</IonText>
             </>
           }
-          {notifications.length == 0 && <>
+          {notifics.length == 0 && <>
               <IonText color="medium" className='pr-2 pt-3 text-center'>Nessuna nuova notifica</IonText>
             </>
           }
         <IonList>
-          {notifications.map((notification, i) => (
+          {notifics.map((notification, i) => (
             <IonItem key={i}>
               <IonLabel>{notification.title}</IonLabel>
               <IonNote slot="end">{pastTime[i]}</IonNote>
