b968bdca61ba7b708bcc61a291c59caa6d6b4347
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-06 14:47:29 +0100

    remove head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Script from 'next/script';
 
 import 'tailwindcss/tailwind.css';
@@ -26,13 +25,6 @@ import { SessionProvider } from 'next-auth/react';
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-        ></meta>
-      </Head>
-
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
