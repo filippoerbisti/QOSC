443e31b639e20282d20bd0318ab5dc74aceae547
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-06 09:25:55 +0100

    formatting

--- next.config.js
+++ next.config.js
@@ -10,9 +10,9 @@ const withTM = require('next-transpile-modules')([
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-  images: {
-    unoptimized: true
-  }
+	images: {
+		unoptimized: true
+	}
 };
 
 module.exports = withTM(nextConfig)
