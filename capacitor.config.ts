e99e0345e1c490f3a8f89fb45ed37c3c34922cb3
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-06 08:45:09 +0100

    add plugin LocalNotifications

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,13 @@ const config: CapacitorConfig = {
 	// 	url: 'http://192.168.116.75:3000',
 	// 	cleartext: true
 	// }
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav",
+    },
+  },
 };
 
 export default config;
