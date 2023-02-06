9d1f4df512c851fadac667bb0d3aa2196fe4fbb0
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-06 14:47:13 +0100

    add head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        ></meta>
+      </Head>
+      
       <body>
         <Main />
         <NextScript />
