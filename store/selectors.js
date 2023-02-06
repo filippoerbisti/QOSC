fd4800212a53f898843ac67ae63b6e7b29976e8e
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-06 10:04:27 +0100

    get new data range

--- store/selectors.js
+++ store/selectors.js
@@ -7,3 +7,6 @@ export const getGroups = createSelector(getState, state => state.groups);
 export const getLists = createSelector(getState, state => state.lists);
 export const getNotifications = createSelector(getState, state => state.notifications);
 export const getSettings = createSelector(getState, state => state.settings);
+
+export const getRangeNotif = createSelector(getState, state => state.pushRangeNotific);
+
