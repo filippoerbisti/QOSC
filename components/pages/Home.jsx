99aa646759c3755b480cb7177b60c196711ab9fb
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-06 10:16:52 +0100

    add check if notification is already sent

--- components/pages/Home.jsx
+++ components/pages/Home.jsx
@@ -211,6 +211,7 @@ const Home = () => {
   const [groupQuery, setGroupQuery] = useState('');
 
   const range = Store.useState(getRangeNotif)
+  const [countAlert, setCountAlert] = useState(0)
 
   useEffect(() => {
     setTimeout(() => {
@@ -221,8 +222,11 @@ const Home = () => {
     let time = new Date().getTime() - new Date(notificationContact.dateLastContact).getTime() - range[1].looptime*60*60*24*1000
     let outTimeHours = (time / (1000 * 60 * 60 * 24)).toFixed(0)
     let name = 'Non contatti ' + capitalizeFirstLetter(notificationContact.name) + ' ' + capitalizeFirstLetter(notificationContact.surname) + ' da ' + outTimeHours + ' giorni oltre il limite'
-    if ((new Date().getTime() - new Date(notificationContact.dateLastContact).getTime()) > (range[1].looptime*60*60*1000))
+
+    if ((new Date().getTime() - new Date(notificationContact.dateLastContact).getTime()) > (range[1].looptime*60*60*1000) && countAlert < 1) {
+      setCountAlert(...[countAlert + 1])
       createNotification(name)
+    }
   }, [loaded, setLoaded])
 
   function capitalizeFirstLetter(string) {
@@ -277,19 +281,20 @@ const Home = () => {
   }
 
   const createNotification = (name) => {
-    LocalNotifications.schedule({
-      notifications: [
-        {
-          title: "Avviso persona da contattare",
-          body: name,
-          id: Math.floor(Math.random() * 6000000),
-          // schedule: {
-          //   at: new Date(Date.now() + 1000 * 10), // in 5 secs
-          //   repeats: true
-          // }
-        }
-      ]
-    });
+    if(countAlert > 1)
+      LocalNotifications.schedule({
+        notifications: [
+          {
+            title: "Avviso persona da contattare",
+            body: name,
+            id: Math.floor(Math.random() * 6000000),
+            // schedule: {
+            //   at: new Date(Date.now() + 1000 * 10), // in 5 secs
+            //   repeats: true
+            // }
+          }
+        ]
+      });
   }
 
   return (
