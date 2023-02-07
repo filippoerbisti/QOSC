57b0a0f29125cc1206dcabb30af3afeae123fbbe
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-02-07 15:32:59 +0100

    getServerSideProps

--- pages/[...all].js
+++ pages/[...all].js
@@ -1,9 +1,26 @@
 import dynamic from 'next/dynamic';
+import Product from '@/models/Product';
+import db from '../utils/db';
+import User from '../models/User';
 
 const App = dynamic(() => import('../components/AppShell'), {
   ssr: false,
 });
 
-export default function Index() {
-  return <App />;
+export default function Index({ users, products }) {
+  return <App users={users} products={products} />;
+}
+
+
+export const getServerSideProps = async () =>  {
+  await db.connect()
+  const products = await Product.find().lean()
+  const users = await User.find().lean()
+
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+      users: users.map(db.convertDocToObj),
+    }
+  }
 }
